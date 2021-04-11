5ba115bbf94703f62ed1899b67709ab337443938
karimovk <karina.karimova@ut.ee>
2021-04-11 12:23:38 +0300

    Creating UI programatically. Passing values between VC

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -35,7 +35,11 @@ class ViewController: UIViewController {
         let weight = weightSlider.value
         
         let bmi = weight/(height*height)
-        print(bmi)
+        
+        let secondVC = SecondViewController()
+        secondVC.bmiValue = String(format: "%0.1f", bmi)
+        self.present(secondVC, animated: true, completion: nil)
+        
     }
     
 }
